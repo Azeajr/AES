9485d58e2c3dd683b91a9062712d55fa748c3e1c
spark343 <spark343@Logan343>
2015-06-16 14:02:01 -0400

    Got it working.  Trying to modularize.

--- AES/Original_AES_CBC_ENC.py
+++ AES/Original_AES_CBC_ENC.py
@@ -4,20 +4,21 @@ from Crypto import Random
 from Crypto.Cipher import AES
 
 
-key = Random.new().read(16)
-K = open('Encryption Key.txt','w')
-K.write(key.encode('hex') + "\n")
+#key = Random.new().read(16)
+#K = open('Encryption Key.txt','w')
+#K.write(key.encode('hex') + "\n")
 
-IV = Random.new().read(16)
-I = open('IV.txt','w')
-I.write(IV.encode('hex') + "\n")
+#IV = Random.new().read(16)
+#I = open('IV.txt','w')
+#I.write(IV.encode('hex') + "\n")
 
-MSGS = []
-ENC_MSGS=[]
-M = open('Messages.txt', 'r')
+#MSGS = []
+#ENC_MSGS=[]
+#M = open('Messages.txt', 'r')
+#m=IV.encode('hex') + M.read().rstrip('\n').encode('hex')
     
-for line in M:
-    MSGS.append(IV.encode('hex') + line.rstrip('\n').encode('hex'))
+#for line in M:
+    #MSGS.append(IV.encode('hex') + line.rstrip('\n').encode('hex'))
 
 #Strings have to be decoded if the string is in hex
 def strxor(a, b):     # xor two strings of different lengths
@@ -26,57 +27,60 @@ def strxor(a, b):     # xor two strings of different lengths
     else:
         return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
-def padd(msg):
+def padd(IV,msg):
     #Number of bytes
-    print "Padding"
-    mess_length=len(msg)/2
-    print "Message length in bytes: " + str(mess_length)
-    
+    mess_length=len(msg)/2    
     IV_len=len(IV.encode('hex'))/2
-    print "IV length in bytes: " + str(IV_len)
     extra_bytes=mess_length%IV_len
-    print "Extra Bytes: " + str(extra_bytes)
     padd_bytes=IV_len-extra_bytes
-    print "Padd Bytes: " + str(padd_bytes)
-    
+        
     for x in range(0,padd_bytes):
         msg+="{:02x}".format(padd_bytes)
 
     return msg
 
-def cbc(previous, next):
+def cbc(key,previous, next):
     temp = strxor(previous.decode('hex'),next.decode('hex'))
     aes = AES.new(key)
     return aes.encrypt(temp).encode('hex')
 
-def encryption(msg):
+def cbc_encryption():
+    
+    key = Random.new().read(16)
+    K = open('Encryption Key.txt','w')
+    K.write(key.encode('hex') + "\n")
+
+    IV = Random.new().read(16)
+    I = open('IV.txt','w')
+    I.write(IV.encode('hex') + "\n")
+
+    #MSGS = []
+    #ENC_MSGS=[]
+    M = open('Messages.txt', 'r')
+    msg=IV.encode('hex') + M.read().rstrip('\n').encode('hex')
+    
+    prepared=padd(IV,msg)
     length = len(msg)
     num_mess = length/len(key.encode('hex'))
  
     temp=msg[0:32]
     for x in range(0,num_mess):
-        temp+=cbc(temp[x*32:(x+1)*32],msg[x*32:(x+1)*32])
+        temp+=cbc(key,temp[x*32:(x+1)*32],msg[x*32:(x+1)*32])
 
     return temp
 
 
 
 def main():
-    print "Message: \t\t" + MSGS[0]    
+     
 
-    print "IV: \t\t\t" + IV.encode('hex')
-    prepared=padd(MSGS[0])
-    print "Prepared Message: \t" + prepared
-    print "KEY: \t\t\t" + key.encode('hex')
-    
-    print "My Encryption: \t\t" + encryption(prepared)
     
     aes=AES.new(key,AES.MODE_CBC, IV)
     print "Python Encryption: \t" + IV.encode('hex') + aes.encrypt(prepared.decode('hex')).encode('hex')
 
-    O = open('Encrypted Text.txt','w')    
-    for msg in MSGS:
-        O.write(encryption(prepared)+ "\n")
+    O = open('Encrypted Text.txt','w')  
+    temp=cbc_encryption()
+    O.write(temp+ "\n")
     
 if __name__ == "__main__":
     main()
