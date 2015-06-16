1fa7363ff9377c6e7c11d0444214270c380ce44c
spark343 <spark343@linusmint>
2015-06-16 19:45:32 -0400

    All working fine.  Streamlined a very little bit.

--- AES/AES_CBC_DEC.py
+++ AES/AES_CBC_DEC.py
@@ -1,15 +1,7 @@
 import sys
 
+from Crypto import Random
 from Crypto.Cipher import AES
-
-K = open('Encryption Key.txt','r')
-key = K.readline().rstrip('\n')
-
-MSGS = []
-M = open('Encrypted Test.txt', 'r')
-    
-for line in M:
-    MSGS.append(line.rstrip('\n'))
 
 #Strings have to be decoded if the string is in hex
 def strxor(a, b):     # xor two strings of different lengths
@@ -18,45 +10,54 @@ def strxor(a, b):     # xor two strings of different lengths
     else:
         return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
-def unpadd(msg):
-    temp=msg
-    padd_bytes = temp[len(temp)-2:len(temp)].decode('hex')
-    
-    return temp[0:len(temp)-ord(padd_bytes)*2]
+def padd(IV,msg):
+    #Number of bytes
+    mess_length=len(msg)/2    
+    IV_len=len(IV.encode('hex'))/2
+    extra_bytes=mess_length%IV_len
+    padd_bytes=IV_len-extra_bytes
+        
+    for x in range(0,padd_bytes):
+        msg+="{:02x}".format(padd_bytes)
 
-def cbc(previous, next):
+    return msg
+
+def cbc(key,previous, next):
     temp = strxor(previous.decode('hex'),next.decode('hex'))
     aes = AES.new(key)
     return aes.encrypt(temp).encode('hex')
 
-def encryption(msg):
+def cbc_encryption():
+    
+    key = Random.new().read(16)
+    K = open('Encryption Key.txt','w')
+    K.write(key.encode('hex') + "\n")
+
+    IV = Random.new().read(16)
+    I = open('IV.txt','w')
+    I.write(IV.encode('hex') + "\n")
+
+    #MSGS = []
+    #ENC_MSGS=[]
+    M = open('Message.txt', 'r')
+    msg=IV.encode('hex') + M.read().rstrip('\n').encode('hex')
+    
+    prepared=padd(IV,msg)
     length = len(msg)
     num_mess = length/len(key.encode('hex'))
-    temp=IV.encode('hex')
-    
+ 
+    temp=msg[0:32]
     for x in range(0,num_mess):
-        temp+=cbc(temp[x*32:(x+1)*32],msg[x*32:(x+1)*32])
+        temp+=cbc(key,temp[x*32:(x+1)*32],msg[x*32:(x+1)*32])
 
     return temp
 
 
 
 def main():
-    print "Message: \t\t" + MSGS[0]
-
-
-    print "IV: \t\t\t" + IV.encode('hex')
-    #addIV(MSGS,0)
-    prepared=padd(IV,MSGS[0])
-    print "Prepared Message: \t" + prepared
-    print "KEY: \t\t\t" + key.encode('hex')
+    O = open('Encrypted Text.txt','w')  
+    temp=cbc_encryption()
+    O.write(temp+ "\n")
     
-    print "My Encryption: \t\t" + encryption(prepared)
-    
-    aes=AES.new(key,AES.MODE_CBC, IV)
-    print "Python Encryption: \t" + IV.encode('hex') + aes.encrypt(prepared.decode('hex')).encode('hex')
-
-
-
 if __name__ == "__main__":
     main()
